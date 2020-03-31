3e80f6a362c199e9112b0a2a5eb329163aeb42df
enevarez1 <ericknevarez97@gmail.com>
2020-03-30 19:43:34 -0600

    Icon uploaded, just need grade screen

--- lib/questionView.dart
+++ lib/questionView.dart
@@ -8,7 +8,7 @@ import 'package:flutter_quiz/user.dart';
 class QuestionView extends StatefulWidget {
 
   final List<Question> questionList;
-  final int counter;
+  int counter;
   final User user;
 
   QuestionView(this.questionList, this.counter, this.user);
@@ -17,10 +17,17 @@ class QuestionView extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return QuestionViewState();
+
+    if(questionList[counter].answerList[0] == 0) {
+      return QuestionViewOpenState();
+    }
+    else {
+      return QuestionViewState();
+    }
   }
 }
 
+//Multiple Choice Questions
 class QuestionViewState extends State<QuestionView> {
 
   
@@ -30,9 +37,11 @@ class QuestionViewState extends State<QuestionView> {
   Widget build(BuildContext context) {
     final _answerList = widget.questionList[widget.counter].answerList;
     bool _currentAnswerValue = false;
+    final _scaffoldKey = GlobalKey<ScaffoldState>();
 
     // TODO: implement build
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text(widget.user.quizNumber),
         centerTitle: true,
@@ -47,7 +56,7 @@ class QuestionViewState extends State<QuestionView> {
                 padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 child: Text(
                   widget.questionList[widget.counter].title,
-                  style: TextStyle(height: 1, fontSize: 20),
+                  style: TextStyle(height: 1, fontSize: 14),
                 ),
               )           
             ),  
@@ -71,13 +80,169 @@ class QuestionViewState extends State<QuestionView> {
                 )).toList(),
             ),
           ),
+          Row (children: <Widget>[
+            IconButton(
+                icon: Icon(Icons.navigate_before),
+                tooltip: 'Move to next question', 
+                onPressed: () {
+                  if (widget.counter != 0) {
+                  widget.counter = widget.counter-1;
+                  print(widget.counter);
+
+                  Navigator.pushReplacement(context, 
+                  MaterialPageRoute(
+                    builder: (context) => QuestionView(widget.questionList, widget.counter, widget.user))
+                  );
+                  }
+                  else {
+                    final snackbar = SnackBar(content: Text("No Previous Question"));
+                    _scaffoldKey.currentState.showSnackBar(snackbar);
+                  }
+
+                } 
+              
+              ),
+              IconButton(
+                icon: Icon(Icons.navigate_next),
+                tooltip: 'Move to next question', 
+                onPressed: () {
+                  if (widget.counter != widget.questionList.length-1) {
+                  print("Pressed Next");
+                  widget.counter = widget.counter+1;
+                  print(widget.counter);
+
+                  Navigator.pushReplacement(context, 
+                  MaterialPageRoute(
+                    builder: (context) => QuestionView(widget.questionList, widget.counter, widget.user))
+                  );
+                  }
+                  else {
+                    final snackbar = SnackBar(content: Text("No Next Question"));
+                    _scaffoldKey.currentState.showSnackBar(snackbar);
+                  }
+
+                } 
+              
+              ),
+          ],)
+        ],
+      ),
+    );
+  }
+}
+
+
+class QuestionViewOpenState extends State<QuestionView> {
+
+  
+
+
+  @override
+  Widget build(BuildContext context) {
+    final _answerList = widget.questionList[widget.counter].answerList;
+    bool _currentAnswerValue = false;
+    final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+    // TODO: implement build
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text(widget.user.quizNumber),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
           Container(
-            color: Colors.indigo,
-            height: 62,
-          )
+            color: Colors.green[300],
+            height: 150,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  widget.questionList[widget.counter].title,
+                  style: TextStyle(height: 1, fontSize: 14),
+                ),
+              )           
+            ),  
+          ),
+          Container(
+            height: 300,
+            child: Padding(
+              padding: EdgeInsets.all(25.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText:'Enter your answer'
+                ),
+              ),
+            ),
+          ),
+          Row (children: <Widget>[
+            IconButton(
+                icon: Icon(Icons.navigate_before),
+                tooltip: 'Move to next question', 
+                onPressed: () {
+                  if (widget.counter != 0) {
+                  widget.counter = widget.counter-1;
+                  print(widget.counter);
+                  Navigator.pushReplacement(context, 
+                    MaterialPageRoute(
+                    builder: (context) => QuestionView(widget.questionList, widget.counter, widget.user))
+                    );
+                  }
+                  else {
+                    final snackbar = SnackBar(content: Text("No Previous Question"));
+                    _scaffoldKey.currentState.showSnackBar(snackbar);
+                  }
+
+                } 
+              
+              ),
+              IconButton(
+                icon: Icon(Icons.navigate_next),
+                tooltip: 'Move to next question', 
+                onPressed: () {
+                  if (widget.counter != widget.questionList.length-1) {
+                  print("Pressed Next");
+                  widget.counter = widget.counter+1;
+                  print(widget.counter);
+
+                  Navigator.pushReplacement(context, 
+                  MaterialPageRoute(
+                    builder: (context) => QuestionView(widget.questionList, widget.counter, widget.user))
+                  );
+                  }
+                  else {
+                    final snackbar = SnackBar(content: Text("No Next Question"));
+                    _scaffoldKey.currentState.showSnackBar(snackbar);
+                  }
+
+                } 
+              
+              ),
+          ],)
         ],
       ),
     );
   }
 }
   
+  /*Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                icon: Icon(Icons.navigate_next),
+                tooltip: 'Move to next question', 
+                onPressed: () {
+                  print("Pressed Next");
+                  widget.counter = widget.counter+1;
+                  print(widget.counter);
+
+                  Navigator.push(context, 
+                  MaterialPageRoute(
+                    builder: (context) => QuestionView(widget.questionList, widget.counter, widget.user))
+                  );
+
+                } 
+              
+              ),
+            ) */
